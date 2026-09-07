753bb48c77728811a5e2e0803fdabc7acb4cc11b
Daniil <dragon.climersid@gmail.com>
2026-09-07 14:29:08 +0700

    Добавьте файлы проекта.

---
+++ Model/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string ModelName { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public double Mileage { get; set; }
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}]" +
+                $" {Brand}" +
+                $" {ModelName}," +
+                $" {Year} г., " +
+                $"{Color}," +
+                $" пробег {Mileage} км," +
+                $" цена {Price} руб.";
+        }
+    }
+}

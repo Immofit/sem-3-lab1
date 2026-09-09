8e0bd022399ffa687f347c837b4c9619a489bebb
Daniil <dragon.climersid@gmail.com>
2026-09-09 10:20:11 +0700

    добавил логик и модель

---
+++ Model/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Model
+{
+    public class Car
+    {
+        public int Id;
+        public string Brand;   
+        public string Model;   
+        public string Color;   
+        public int Year;       
+
+        public Car(int id, string brand, string model, string color, int year)
+        {
+            Id = id;
+            Brand = brand;
+            Model = model;
+            Color = color;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return "Id=" + Id + " | " + Brand + " " + Model + " | Цвет: " + Color + " | " + Year + " год";
+        }
+    }
+}
